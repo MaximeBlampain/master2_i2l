b1beaed71e69b155a884584298504ffa446126f3
Maxime Blampain <mblampain@pop-os.localdomain>
2021-09-11 10:28:52 +0200

    ApplicationsMobile - TP2 Terminé et envoyé au prof

--- ApplicationsMobile/Tps/TP2/app/src/main/java/com/example/calcblampain/MainActivity.java
+++ ApplicationsMobile/Tps/TP2/app/src/main/java/com/example/calcblampain/MainActivity.java
@@ -6,10 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    String calculus = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +18,61 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onExitApp(View view) {
+        MainActivity.this.finish();
     }
 
     public void onExecuteCalculus(View view) {
+        // Get the input fields
+        EditText inputOne = (EditText) findViewById(R.id.NumberInputOne);
+        EditText inputTwo = (EditText) findViewById(R.id.NumberInputTwo);
+
+        String txtInputOne = inputOne.getText().toString();
+        String txtInputTwo = inputTwo.getText().toString();
+
+        // Get the checked radio button
+        RadioGroup rdioGrpOperations = (RadioGroup) findViewById(R.id.operand_group);
+
+        // Get the result field to display output
+        TextView txtResult = (TextView) findViewById(R.id.ResultCalc);
+
+        if(txtInputOne.equals("") || txtInputTwo.equals("")){
+            txtResult.setText(getString(R.string.empty_field));
+            return;
+        }
+        int valueOne = Integer.parseInt(inputOne.getText().toString());
+        int valueTwo = Integer.parseInt(inputTwo.getText().toString());
+
+        int checkedId = rdioGrpOperations.getCheckedRadioButtonId();
+
+        if(checkedId == -1) txtResult.setText(getString(R.string.no_operator_selected));
+        else {
+            RadioButton rdioChecked = (RadioButton) findViewById(checkedId);
+
+            // Check the checked type to make calculus
+            double finalResult;
+            switch (rdioChecked.getText().toString()){
+                case "Plus":
+                    finalResult = valueOne + valueTwo;
+                    txtResult.setText(String.valueOf(finalResult));
+                    break;
+                case "Moins":
+                    finalResult = valueOne - valueTwo;
+                    txtResult.setText(String.valueOf(finalResult));
+                    break;
+                case "Multiplié":
+                    finalResult = valueOne * valueTwo;
+                    txtResult.setText(String.valueOf(finalResult));
+                    break;
+                case "Divisé":
+                    finalResult = valueOne / valueTwo;
+                    txtResult.setText(String.valueOf(finalResult));
+                    break;
+                default :
+                    txtResult.setText(getString(R.string.unknown_operation));
+                    break;
+            }
+        }
+
     }
 
     public void onResetCalculus(View view) {
@@ -45,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         radioDiviser.setChecked(false);
 
         // Get result text field
-        TextView txtResult = (TextView) findViewById(R.id.ResultLabel);
+        TextView txtResult = (TextView) findViewById(R.id.ResultCalc);
 
         // Reset result field
         txtResult.setText("");
