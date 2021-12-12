d9c7b3599e82436133f1af83322d6e8e4a836cf9
Maxime Blampain <mblampain@outlook.fr>
2021-12-12 21:22:59 +0100

    add cours réseau

---
+++ POOA/TPs/TP_miniPaint/minipaint/src/main/java/drawing/commands/ShapeCommand.java
@@ -0,0 +1,18 @@
+package drawing.commands;
+
+import drawing.ui.DrawingPane;
+
+public class ShapeCommand implements ICommand{
+
+    public ShapeCommand(DrawingPane drawingPane, )
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void undo() {
+
+    }
+}
